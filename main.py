45b4a55027c333682f0cca3edc12224ae12f6dc7
Gabriel Cirio <gabriel.cirio@seddi.com>
2026-01-05 15:10:57 +0100

    tags for avoiding bots

--- main.py
+++ main.py
@@ -476,6 +476,7 @@ def generate_html_index():
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
     <title>Readerton - PDF Archive</title>
     <style>
         body {
